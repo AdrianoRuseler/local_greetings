3b8639454da4d35a73b1d9c629b980845cfbffb5
Adriano Ruseler <adrianoruseler@gmail.com>
2024-01-19 17:31:20 -0300

    Update version.php
    Preventing direct access to certain files

--- version.php
+++ version.php
@@ -22,6 +22,15 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+
+ // Preventing direct access to certain files
+ // Some files cannot be accessed directly by users through the web browser.
+ // Moodle handles this through the use of the code given below.
+ // This code ensures that if this file is being directly accessed,
+ // it will just stop executing after the first line.
+ // Such files are usually accessed through another file or internal APIs,
+ // where the pre-conditions for accessing such files are already defined/loaded.
+
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_greetings';
