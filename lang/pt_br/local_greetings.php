d9bf8ffb23e2236e6bd9028911ec1919b398a030
Adriano Ruseler <adrianoruseler@gmail.com>
2024-01-26 13:12:18 -0300

    Create local_greetings.php

---
+++ lang/pt_br/local_greetings.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_greetings
+ * @category    string
+ * @copyright   2024 Adriano Ruseler <adrianoruseler@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Greetings';
+$string['greetinguser'] = 'Greetings, DEFAULT user.';
+$string['greetingloggedinuser'] = 'Greetings, logged {$a}.';
+$string['greetingnotloggedinuser'] = 'Greetings, NOT logged {$a}.';
+
+// Display greetings based on user's country.
+$string['greetingusernull'] = 'Greetings, NULL user.';
+$string['greetinguserptbr'] = 'Olá, {$a}.';
+$string['greetinguserau'] = 'Hello, {$a}.';
+$string['greetinguseren'] = 'Hello, {$a}.';
+$string['greetinguseres'] = 'Hola, {$a}.';
+$string['greetinguserfj'] = 'Bula, {$a}.';
+$string['greetingusernz'] = 'Kia Ora, {$a}.';
+
+// Create the form.
+$string['yourmessage'] = 'Your message';
+
+// Display user details with greetings posts.
+$string['postedby'] = 'Postado por {$a}.';
